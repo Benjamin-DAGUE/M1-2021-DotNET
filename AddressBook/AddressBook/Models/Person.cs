1b7218134dcbf62f59738278edb8ea94640b2c4d
Benjamin BD. DAGUE <benjamin.dague@etskirsch.fr>
2021-09-13 11:47:10 +0200

    Commit initial du programme AddressBook

---
+++ AddressBook/AddressBook/Models/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    /// <summary>
+    ///     Représente une personne.
+    /// </summary>
+    public class Person
+    {
+        #region Fields (Attributs)
+
+        /// <summary>
+        ///     Nom de famille de la personne.
+        /// </summary>
+        private string _FirstName;
+
+        /// <summary>
+        ///     Date de naissance de la personne.
+        /// </summary>
+        private DateTime _Birthdate;
+
+        #endregion
+
+        #region Properties (Accesseurs / propriétés)
+
+        /// <summary>
+        ///     Obtient ou définit le nom de famille de la personne.
+        /// </summary>
+        /// <remarks>
+        ///     Cette version expose un attribut définit dans la classe. Elle a l'avantage de permettre de personaliser le comportement des méthodes get et set.
+        /// </remarks>
+        public string FirstName { get => this._FirstName; set => this._FirstName = value; }
+
+        /// <summary>
+        ///     Obtient ou définit le prénom de la personne.
+        /// </summary>
+        /// <remarks>
+        ///     Avec cette syntaxe, il n'est pas nécessaire de déclarer un attribut et d'écrir les méthodes get et set;
+        /// </remarks>
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///     Obtient ou définit la date de naissance de la personne.
+        /// </summary>
+        /// <remarks>
+        ///     Cette syntaxe est la plus verbeuse, son comportement est similaire à la propriété <see cref="FirstName"/>.
+        /// </remarks>
+        public DateTime Birthdate
+        {
+            get { return _Birthdate; }
+            set { _Birthdate = value; }
+        }
+
+        /// <summary>
+        ///     Obtient le nom complet de la personne.
+        /// </summary>
+        /// <remarks>
+        ///     Cette syntaxe permet de créer une propriété en lecture seule.
+        /// </remarks>
+        public string FullName => this.FirstName + " " + this.LastName;
+
+        /// <summary>
+        ///     Obtient ou définit l'adresse email de la personne.
+        /// </summary>
+        public string EMailAddress { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        //Méthodes
+
+        //constructeur / destructeur
+
+        //Événements
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AddressBook.Models;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            //1 - Afficher le menu suivant :
+            // 1 : Parcourir le carnet
+            // 2 : Ajouter une personne
+            // 3 : Rechercher une personne
+            // 0 : Quitter
+
+            // 1 : Parcourir permet d'afficher la première personne puis propose à l'utilisateur les options :
+            //      1 - Suivant
+            //      2 - Précédent
+            //      3 - Supprimer
+            //      0 - Menu principal
+
+            // 2 : L'ajout d'une personne demande à l'utilisateur les informations 
+
+            // 3 : Implémenter une fonction de recherche. Présenter les résultats de la même manière que le menu "Parcourir"
+
+            // 4 : Sauvegarder le carner dans un fichier à la fermeture du programme, au démarrage, charger le fichier.
+                    // JSON
+
+        }
+    }
+}
