af99f5b80933470649836f13ef10031be768e29b
Benjamin BD. DAGUE <benjamin.dague@etskirsch.fr>
2021-09-14 10:12:48 +0200

    Ajout du cours sur les delegates

---
+++ CoursDelegate/CoursDelegate/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace CoursDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action et Func sont des délégués génériques qui sont utilisés pour créer des références vers une méthode.
+            //On peut affecter une référence à une méthode existante de la classe
+            Action<string> methodReturnVoid = WriteWarningMessage;
+            //Puis l'appeler 
+            methodReturnVoid("Message à afficher");
+            //On peut également créer une méthode anonyme inline
+            methodReturnVoid = (msg) => Console.WriteLine(msg);
+            //Puis l'appeler 
+            methodReturnVoid("Message à afficher");
+
+            //Le système de délégués permet de créer des événéments ou des callbacks
+
+            DoWorkWithCallback();
+            System.Threading.Thread.Sleep(2000);
+
+            Console.Clear();
+            DoWorkWithEvent();
+        }
+
+        static void WriteWarningMessage(string message)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+
+        static void DoWorkWithCallback()
+        {
+            object isEndedLocker = new object();//objet utilisé pour vérouiller l'accès à la variable isEnded et éviter un accès concurentiel (modification/lecture par deux threads simultanément).
+            bool isEnded = false;
+
+            Worker worker = new Worker();
+
+            Console.WriteLine("Démarrage du traitement async avec un callback");
+
+            worker.DoWorkWithCallbackAsync(() =>
+            {
+                lock (isEndedLocker) //On attend la libération du vérou puis on vérouille l'accès à la variable isEnded.
+                {
+                    isEnded = true; //On marque le traitement comme terminé pour terminer le programme.
+                }
+            });
+
+            Console.WriteLine("Traitement en cours");
+
+            bool exitLoop = false;
+
+            do
+            {
+                lock (isEndedLocker) //On attend la libération du vérou puis on vérouille l'accès à la variable isEnded.
+                {
+                    exitLoop = isEnded; //On copie dans une variable temporaire la valeur de la variable isEnded.
+                }
+                System.Threading.Thread.Sleep(100); //On attend quelques instants pour éviter de surcharger le thread principal.
+            } while (exitLoop == false);
+
+            Console.WriteLine("Traitement terminé");
+        }
+
+        static void DoWorkWithEvent()
+        {
+            object isEndedLocker = new object(); //objet utilisé pour vérouiller l'accès à la variable isEnded et éviter un accès concurentiel (modification/lecture par deux threads simultanément).
+            bool isEnded = false;
+
+            Worker worker = new Worker();
+            worker.WorkEnded += (sender, e) =>
+            {
+                lock (isEndedLocker) //On attend la libération du vérou puis on vérouille l'accès à la variable isEnded.
+                {
+                    isEnded = true; //On marque le traitement comme terminé pour terminer le programme.
+                }
+            };
+
+            Console.WriteLine("Démarrage du traitement async avec un événement");
+            
+            worker.DoWorkWithEventAsync();
+
+            Console.WriteLine("Traitement en cours");
+
+            bool exitLoop = false;
+
+            do
+            {
+                lock (isEndedLocker) //On attend la libération du vérou puis on vérouille l'accès à la variable isEnded.
+                {
+                    exitLoop = isEnded; //On copie dans une variable temporaire la valeur de la variable isEnded.
+                }
+                System.Threading.Thread.Sleep(100); //On attend quelques instants pour éviter de surcharger le thread principal.
+            } while (exitLoop == false);
+
+            Console.WriteLine("Traitement terminé");
+        }
+    }
+}
---
+++ CoursDelegate/CoursDelegate/Worker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursDelegate
+{
+    class Worker
+    {
+        #region Implémentation avec un événement
+
+        /// <summary>
+        ///     Déclenché lorsque le traitement asynchrone est terminé.
+        /// </summary>
+        public event EventHandler WorkEnded; //Création d'un événement. Un événement est une liste d'abonnée à rappeler lorsque l'objet change d'état (par exemple dans notre cas lorsque le traitement est terminé).
+
+        /// <summary>
+        ///     Effectue un travail asynchrone et déclenche l'événement <see cref="WorkEnded"/> à la fin du traitement.
+        /// </summary>
+        public void DoWorkWithEventAsync()
+        {
+            Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(5000);        //Attente de 5 secondes.
+                WorkEnded?.Invoke(this, new EventArgs());   //Si on a des abonnonés, on appel les abonnés.
+            });
+        }
+
+        #endregion
+
+        #region Implémentation avec un callback
+
+        /// <summary>
+        ///     Effectue un travail asynchrone et déclenche la méthode <see cref="endedCallback"/> passée en paramètre.
+        /// </summary>
+        /// <param name="endedCallback">Référence d'une méthode à appeler à la fin du traitement.</param>
+        public void DoWorkWithCallbackAsync(Action endedCallback)
+        {
+            Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(5000);    //Attente de 5 secondes.
+                endedCallback?.Invoke();                //Si on a une référence de méthode en paramètre, on appel la méthode.
+            });
+        }
+
+        #endregion
+    }
+}
